90da8d3dec101130bb73cc3408b1b6f5a2a2f0ad
Jose3212 <Danteose1@gmail.com>
2016-09-25 19:34:16 +0200

    Commit 1

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -22,8 +22,8 @@ void MainWindow::on_pushButton_clicked()
     QMediaPlayer *player = new QMediaPlayer;
 
     QMediaPlaylist *playlist = new QMediaPlaylist(player);
-    if(playlist->addMedia(QUrl::fromLocalFile("/home/jose/Proyecto1/1.mp4"))){
-        if(playlist->addMedia(QUrl::fromLocalFile("/home/jose/Proyecto1/2.mp4"))){
+    if(playlist->addMedia(QUrl::fromLocalFile("1.mp4"))){
+        if(playlist->addMedia(QUrl::fromLocalFile("2.mp4"))){
             QVideoWidget *videoWidget = new QVideoWidget;
             player->setVideoOutput(videoWidget);
             videoWidget->showFullScreen();
